90672e6f7edb4ccf5ffffdf15e1720506f4fc434
Justin Woo <moomoowoo@gmail.com>
2016-05-27 22:00:58 +0300

    fix(Types): fix data type signatures and add Eff (#2)

--- src/Almost.purs
+++ src/Almost.purs
@@ -1,19 +1,20 @@
 module Almost where
 
 import Data.Maybe (Maybe)
+import Control.Monad.Eff (Eff, Pure)
 
 newtype Undefined a = Undefined (Maybe a)
-newtype Stream a = Stream a
-newtype Subject a = Subject a
 
+foreign import data Subject :: * -> *
+foreign import data Stream :: * -> *
 foreign import data Promise :: * -> *
 
 {-Sad subject type stuff -}
 foreign import subject :: forall a. Subject a
 foreign import holdSubject :: forall a. Int -> Subject a
 
-foreign import next :: forall a. a -> Subject a -> Subject a
-foreign import complete :: forall a. a -> Subject a -> Subject a
+foreign import next :: forall a. a -> Subject a -> Pure (Subject a)
+foreign import complete :: forall a. a -> Subject a -> Pure (Subject a)
 
 {- create streams -}
 
@@ -23,7 +24,7 @@ foreign import never :: forall a. a -> Stream (Undefined a)
 foreign import periodic :: Int -> forall a. a -> Stream a
 
 {- consume streams -}
-foreign import observe :: forall a b. (a -> b) -> Stream a -> Promise a
+foreign import observe :: forall a b eff. (a -> Eff eff b) -> Stream a -> Promise a
 foreign import drain :: forall a. Stream a -> Promise a
 {- then a Promise -}
 foreign import thenp :: forall a b. (a -> b) -> Promise a -> Promise b
